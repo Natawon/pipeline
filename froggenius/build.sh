89d32ddcbf5643b37a91b8e60852a023ed7bc814
“Natawon” <natawon9@gmail.com>
2023-03-21 12:09:47 +0700

    add annsible

--- froggenius/build.sh
+++ froggenius/build.sh
@@ -4,4 +4,4 @@
 
 # ansible-playbook -i ./hosts main.yaml --extra-vars "domain=test.froggenius.com project=test http_port=8001 https_port=9001" -vv
 
-ansiblePlaybook disableHostKeyChecking: true, extras: 'domain=test.froggenius.com project=test http_port=8001 https_port=9001', installation: 'ansible', inventory: 'host', playbook: 'main.yml'
+ansible-playbook disableHostKeyChecking: true, extras: 'domain=test.froggenius.com project=test http_port=8001 https_port=9001', installation: 'ansible', inventory: 'host', playbook: 'main.yml'
