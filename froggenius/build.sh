e0e7281529079a58cf05333ef0243ee990e9ea97
“Natawon” <natawon9@gmail.com>
2023-03-21 17:27:09 +0700

    edit file

--- froggenius/build.sh
+++ froggenius/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Hello World"
+ echo "Hello World"
 
 # ansible-playbook -i hosts main.yaml --extra-vars "domain=test.froggenius.com project=test http_port=8001 https_port=9001" -vvv
 
