c50d3e2fbb6bbf1bf8b2c0cacfde7f810943ecec
“Natawon” <natawon9@gmail.com>
2023-03-21 18:29:32 +0700

    edit file

--- froggenius/build.sh
+++ froggenius/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
- echo "Hello World"
+#  echo "Hello World"
 
-# ansible-playbook -i hosts main.yaml --extra-vars "domain=test.froggenius.com project=test http_port=8001 https_port=9001" -vvv
+ansible-playbook -i ./hosts main.yaml --extra-vars "domain=test.froggenius.com project=test http_port=8001 https_port=9001" -vvv
 
 # ansible-playbook disableHostKeyChecking: true, extras: 'domain=test.froggenius.com project=test http_port=8001 https_port=9001', installation: 'ansible', inventory: 'host', playbook: 'main.yml'
